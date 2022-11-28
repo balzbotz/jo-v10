ebde354f8aed932610f0ad3ff9bbf0e9d8d5e509
balzbotz <93869984+balzbotz@users.noreply.github.com>
2022-11-28 18:24:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'BALZ BOT BUG'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6285743137623','6285743137623','6285743137623']
+global.premium = ['6285743137623']
+global.packname = 'Bot bug'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
